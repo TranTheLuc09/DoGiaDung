f8cc917c0c54b7b2ac40c818ef52359b342bbe26
TranTheLuc09 <utlinh2k3@gmail.com>
2023-10-16 23:55:27 +0700

    update

---
+++ Api.BanHang/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DataModel;
+using BusinessLogicLayer;
+using BusinessLogicLayer.Interfaces;
+
+namespace Api.BanHang.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        IAccountBusiness AccountBusiness;
+        public AccountController(IAccountBusiness accountBusiness)
+        {
+            AccountBusiness = accountBusiness;
+        }
+
+        [HttpGet("getListAccount")]
+        public List<Account> GetListAccount()
+        {
+            var result = AccountBusiness.GetListAccount();
+            return result;
+        }
+
+    }
+}
---
+++ DataAccessLayer/AccountRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Interfaces;
+using DataModel;
+
+namespace DataAccessLayer
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private IDatabaseHelper _dbHelper;
+
+        public AccountRepository(IDatabaseHelper dbHelper)
+        {
+            _dbHelper = dbHelper;
+        }
+        public List<Account> GetListAccount()
+        {
+            string msgError = "";
+            try
+            {
+                var dt = _dbHelper.ExecuteSProcedureReturnDataTable(out msgError, "getlistacount");
+                if (!string.IsNullOrEmpty(msgError))
+                    throw new Exception(msgError);
+                return dt.ConvertTo<Account>().ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
